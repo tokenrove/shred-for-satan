696fd4b9429713590dde87a7ddb577e427408aa8
Julian Squires <julian@cipht.net>
2011-06-19 20:14:03 -0400

    Fix transitions between bars of different durations
    The measure wasn't getting changed over at the right point.  Also did
    some refactoring to clean up the metronome code and hopefully avoid
    race conditions causing erroneous bar beginnings.  Note that the
    problem of the first note not being played is still present here.

--- metronome.ml
+++ metronome.ml
@@ -1,16 +1,10 @@
 open Bigarray
 
-let may f x = match x with
-  | (Some x) -> f x | None -> ()
+let may f x = match x with | (Some x) -> f x | None -> ()
 
 let stream = ref None
 
-let buflen = 110
-let bufc = Array.make buflen 0.
-let buf = [| bufc; |]
-
 let samples_per_sec = 44100.
-let beep_duration = 0.04
 let two_pi = 4. *. (asin 1.)
 let volume = ref 0.375
 type state = Tick | Fade | Rest
@@ -19,7 +13,6 @@ let duration = ref 0			(* samples *)
 
 let tick_duration = 0.04		(* seconds *)
 let fade_samples = 10			(* samples *)
-let pitch = ref 607.
 let period = ref 109.
 let measure_p = ref 0
 let new_measure_fn = ref (fun () -> [| (0.,1.) |])
@@ -28,36 +21,39 @@ let measure = ref (!new_measure_fn ())
 let inner_vol = ref 0.
 
 let start fn =
-  may Portaudio.start_stream !stream;
   new_measure_fn := fn;
+  measure := (!new_measure_fn) ();
+  measure_p := 0;
   state := Tick;
   inner_vol := !volume;
   duration := (truncate (samples_per_sec *. tick_duration)) - fade_samples;
-  period := samples_per_sec /. !pitch;
-  measure := (!new_measure_fn) ();
-  measure_p := 0
+  let pitch = (fst (!measure).(!measure_p)) in
+  period := if pitch = 0. then 0. else samples_per_sec /. pitch;
+  may Portaudio.start_stream !stream
 
-let bufp = ref 0
-let last_val = ref 0.
+let sine_wave () = sin (two_pi *. ((mod_float (float_of_int !duration) !period) /. (!period -. 1.)))
+
+let next_bar () =
+  incr measure_p;
+  if !measure_p >= Array.length !measure then begin
+    measure_p := 0;
+    measure := (!new_measure_fn) ();
+  end
+
 
 let update _ out l =
   let set i v = Genarray.set out [|i|] v in
   for i = 0 to pred l do
-    set i (if !period = 0. then 0. else (!inner_vol *. sin (two_pi *. ((mod_float (float_of_int !duration) (!period -. 1.)) /. !period))));
+    set i (if !period = 0. then 0. else (!inner_vol *. sine_wave ()));
     (match !state with | Fade -> inner_vol := !inner_vol *. 0.01 | _ -> () );
     decr duration;
     if (!duration) <= 0 then
       (match !state with
 	| Tick -> state := Fade; duration := fade_samples;
 	| Fade -> state := Rest; inner_vol := 0.;
-	  incr measure_p;
-	  if !measure_p >= Array.length !measure then begin
-	    measure_p := 0;
-	    measure := (!new_measure_fn) ();
-	  end;
 	  let t = (snd (!measure).(!measure_p)) in
 	  duration := truncate ((samples_per_sec *. t) -. tick_duration)
-	| Rest -> state := Tick; inner_vol := !volume;
+	| Rest -> state := Tick; inner_vol := !volume; next_bar ();
 	  duration := (truncate (samples_per_sec *. tick_duration)) - fade_samples;
 	  let pitch = (fst (!measure).(!measure_p)) in
 	  period := if pitch = 0. then 0. else samples_per_sec /. pitch);
@@ -73,12 +69,3 @@ let init () =
 
 let stop () =
   may Portaudio.stop_stream !stream
-
-(* 
- * let _ =
- *   init ();
- *   start measure;
- *   (\*update 5.0;*\)
- *   Unix.sleep 5;
- *   stop ();
- *)
