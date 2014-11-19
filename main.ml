c14b980de0dd072d06433db6de7914c2e12c2c31
Julian Squires <julian@cipht.net>
2014-11-19 09:07:33 -0500

    Refactor main in trivial ways
    A one-day hack can cost a lot after the fact.  This needs much more
    aggressive refactoring (and tests) but these minor cleanups are a start.

--- main.ml
+++ main.ml
@@ -4,75 +4,116 @@ let locale = GMain.Main.init ()
 let midi_state = ref []
 let fifth root = root *. (2.**(7./.12.))
 let root_pitch = ref 440.
-let meter = ref {Midi.numerator=4; Midi.denominator=4;}
+let meter = ref Midi.({numerator=4; denominator=4;})
 let tempo = ref 120.
+
+let construct_measure m speed_factor =
+  let open Midi in
+  let {numerator; denominator} = m in
+  let dur = ((60. /. !tempo) /. ((float_of_int denominator) /. 4.)) /. speed_factor in
+  let a = Array.make numerator (!root_pitch,dur) in
+  a.(0) <- (fifth !root_pitch,dur);
+  a
+
+type preroll_box = {bars:GData.adjustment; numerator:GData.adjustment; denominator:GData.adjustment}
+
+let make_preroll packing =
+  let frame = GBin.frame ~label:"Preroll:" ~packing ~border_width:10 () in
+  let hbox = GPack.hbox ~packing:frame#add ~border_width:10 () in
+  let bars = GData.adjustment ~lower:0. ~value:2. ~page_size:0. () in
+  let packing = (hbox#pack ~expand:false) in
+  GEdit.spin_button ~adjustment:bars ~packing () |> ignore;
+  GMisc.label ~text:"bars of" ~packing () |> ignore;
+  let numerator = GData.adjustment ~lower:1. ~value:4. ~page_size:0. () in
+  let denominator = GData.adjustment ~lower:1. ~value:4. ~page_size:0. () in
+  let _ = GEdit.spin_button ~adjustment:numerator ~packing () in
+  let _ = GMisc.label ~text:"/" ~packing:(hbox#pack ~expand:false) () in
+  let _ = GEdit.spin_button ~adjustment:denominator ~packing () in
+  {bars; numerator; denominator}
+
+let make_speed_box packing =
+  let frame = GBin.frame ~label:"Speed:" ~packing ~border_width:10 () in
+  let hbox = GPack.hbox ~packing:frame#add () in
+  let speed_factor = GData.adjustment ~lower:0.001 ~upper:2. ~value:1. ~step_incr:0.01 ~page_size:0. () in
+  GRange.scale `HORIZONTAL ~value_pos:`RIGHT ~adjustment:speed_factor ~digits:3 ~packing:(hbox#pack ~expand:true ~padding:10) |> ignore;
+  speed_factor
+
+type position_labels = {key:GMisc.label; meter:GMisc.label; tempo:GMisc.label;}
+let make_position_box packing =
+  let hbox = GPack.hbox ~packing ~border_width:10 () in
+  let position = GData.adjustment ~lower:1. ~upper:666. ~value:1. ~page_size:0. () in
+  let packing = (hbox#pack ~expand:true) in
+  GRange.scale `HORIZONTAL ~draw_value:false ~adjustment:position ~packing () |> ignore;
+  let _ = GMisc.label ~text:"Bar:" ~packing () in
+  let _ = GEdit.spin_button ~adjustment:position ~packing () in
+  let packing = (hbox#pack ~expand:false ~padding:10) in
+  let key = GMisc.label ~text:"(key)" ~packing () in
+  let meter = GMisc.label ~text:"4/4" ~packing () in
+  let tempo = GMisc.label ~text:"♩ = 120" ~packing () in
+  (position, {key; meter; tempo})
+
+let construct_file_menu menu_bar load_file =
+  let file_item = GMenu.menu_item ~label:"File" ~packing:menu_bar#append () in
+  let file_menu = GMenu.menu ~packing:file_item#set_submenu () in
+  let packing = file_menu#append in
+  let open_fn () =
+    let chooser = GWindow.file_selection ~title:"Choose MIDI file" () in
+    ignore (chooser#ok_button#connect#clicked ~callback:(fun () -> load_file chooser#filename; chooser#destroy ()));
+    ignore (chooser#cancel_button#connect#clicked ~callback:chooser#destroy);
+    chooser#show () in
+  let quit_fn = GMain.Main.quit in
+  List.iter (fun (l,f) -> let i = GMenu.menu_item ~label:l ~packing () in
+                          ignore (i#connect#activate ~callback:f))
+    [("Open", open_fn); ("Quit", quit_fn)]
 
 let main () =
   let window = GWindow.window ~title:"Shred for Satan" () in
   ignore (window#connect#destroy ~callback:GMain.Main.quit);
   let vbox = GPack.vbox ~packing:window#add () in
   let menu_bar = GMenu.menu_bar ~packing:vbox#pack () in
-  let file_item = GMenu.menu_item ~label:"File" ~packing:menu_bar#append () in
-  let file_menu = GMenu.menu ~packing:file_item#set_submenu () in
 
-  let file_label = GMisc.label ~text:"No file loaded.  (Use Open from the File menu)"
-    ~packing:(vbox#pack ~expand:false) () in
-  let _ = GMisc.separator `HORIZONTAL ~packing:(vbox#pack ~expand:false ~padding:5) () in
+  let packing = (vbox#pack ~expand:false ~padding:5) in
+  let file_label = GMisc.label ~packing
+      ~text:"No file loaded.  (Use Open from the File menu)" () in
+  GMisc.separator `HORIZONTAL ~packing () |> ignore;
 
-  let frame = GBin.frame ~label:"Preroll:" ~packing:(vbox#pack ~expand:false ~padding:5) ~border_width:10 () in
-  let hbox = GPack.hbox ~packing:frame#add ~border_width:10 () in
-  let preroll_bars = GData.adjustment ~lower:0. ~value:2. ~page_size:0. () in
-  let _ = GEdit.spin_button ~adjustment:preroll_bars ~packing:(hbox#pack ~expand:false) () in
-  let _ = GMisc.label ~text:"bars of" ~packing:(hbox#pack ~expand:false) () in
-  let preroll_numerator = GData.adjustment ~lower:1. ~value:4. ~page_size:0. () in
-  let preroll_denominator = GData.adjustment ~lower:1. ~value:4. ~page_size:0. () in
-  let _ = GEdit.spin_button ~adjustment:preroll_numerator ~packing:(hbox#pack ~expand:false) () in
-  let _ = GMisc.label ~text:"/" ~packing:(hbox#pack ~expand:false) () in
-  let _ = GEdit.spin_button ~adjustment:preroll_denominator ~packing:(hbox#pack ~expand:false) () in
+  let preroll = make_preroll packing in
+  let speed_factor = make_speed_box packing in
 
-  let frame = GBin.frame ~label:"Speed:" ~packing:(vbox#pack ~expand:false ~padding:5) ~border_width:10 () in
-  let hbox = GPack.hbox ~packing:frame#add () in
-  let speed_factor = GData.adjustment ~lower:0.001 ~upper:2. ~value:1. ~step_incr:0.01 ~page_size:0. () in
-  let _ = GRange.scale `HORIZONTAL ~value_pos:`RIGHT ~adjustment:speed_factor ~digits:3 ~packing:(hbox#pack ~expand:true ~padding:10) () in
+  GMisc.separator `HORIZONTAL ~packing () |> ignore;
 
-  let _ = GMisc.separator `HORIZONTAL ~packing:(vbox#pack ~expand:false ~padding:5) () in
-
-  let hbox = GPack.hbox ~packing:(vbox#pack ~expand:false) ~border_width:10 () in
-  let position = GData.adjustment ~lower:1. ~upper:666. ~value:1. ~page_size:0. () in
-  let _ = GRange.scale `HORIZONTAL ~draw_value:false ~adjustment:position ~packing:(hbox#pack ~expand:true) () in
-  let _ = GMisc.label ~text:"Bar:" ~packing:(hbox#pack ~expand:false) () in
-  let _ = GEdit.spin_button ~adjustment:position ~packing:(hbox#pack ~expand:false) () in
-  let key_label = GMisc.label ~text:"(key)" ~packing:(hbox#pack ~expand:false ~padding:10) () in
-  let meter_label = GMisc.label ~text:"4/4" ~packing:(hbox#pack ~expand:false ~padding:10) () in
-  let tempo_label = GMisc.label ~text:"♩ = 120" ~packing:(hbox#pack ~expand:false ~padding:10) () in
-
-  let play_btn = GButton.toggle_button ~stock:`MEDIA_PLAY ~packing:(vbox#pack ~expand:false) () in
+  let packing = (vbox#pack ~expand:false) in
+  let position, labels = make_position_box packing in
+  let play_btn = GButton.toggle_button ~stock:`MEDIA_PLAY ~packing () in
   play_btn#misc#set_sensitive false;
 
   let update_labels_for_position bar = begin
-    let nearest_to_bar p blank = List.fold_left (fun a (m,b) -> if m <= bar && (p b) then (m,b) else a) (0,blank) !midi_state in begin
-      let blank = {Midi.numerator=4;Midi.denominator=4;} in
+    let open Midi in
+    let nearest_to_bar p blank =
+      List.fold_left (fun a (m,b) -> if m <= bar && (p b) then (m,b) else a)
+        (0,blank) !midi_state
+    in begin
+      let blank = {numerator=4;denominator=4;} in
       meter := (match
-	  (nearest_to_bar (function | Midi.Meter _ -> true | _ -> false) (Midi.Meter blank))
-	with | (_,Midi.Meter m) -> m | _ -> blank);
-      tempo := (match (nearest_to_bar (function | Midi.Tempo _ -> true | _ -> false) (Midi.Tempo 120.))
-	with | (_,Midi.Tempo t) -> t | _ -> 120.);
-      let blank = (Midi.MajorKey 0) in
-      let key = (match (nearest_to_bar (function | Midi.Key _ -> true | _ -> false) (Midi.Key blank))
-	with | (_,Midi.Key k) -> k | _ -> blank) in
-      root_pitch := Midi.root_pitch_of_key key;
-      key_label#set_label (Midi.pretty_print_key key);
-      meter_label#set_label ((string_of_int (!meter).Midi.numerator)^"/"^(string_of_int (!meter).Midi.denominator));
-      let l = if speed_factor#value <> 1.0 then
-	  ("♩ = "^ (string_of_int (truncate (!tempo *. speed_factor#value))) ^ " ("^ (string_of_int (truncate !tempo))^")")
-	else
-	  ("♩ = "^ (string_of_int (truncate !tempo)))
-      in
-      tempo_label#set_label l
+                  (nearest_to_bar (function | Meter _ -> true | _ -> false) (Meter blank))
+                with | (_,Meter m) -> m | _ -> blank);
+      tempo := (match (nearest_to_bar (function | Tempo _ -> true | _ -> false) (Tempo 120.))
+         with | (_,Tempo t) -> t | _ -> 120.);
+      let blank = (MajorKey 0) in
+      let key = match (nearest_to_bar (function | Key _ -> true | _ -> false) (Key blank))
+        with | (_,Key k) -> k | _ -> blank in
+      root_pitch := root_pitch_of_key key;
+      labels.key#set_label (pretty_print_key key);
+      let {numerator;denominator} = !meter in
+      labels.meter#set_label (Printf.sprintf "%d/%d" numerator denominator);
+      let is_adjusted = speed_factor#value <> 1.0 in
+      labels.tempo#set_label
+        (Printf.sprintf "♩ = %d%s" (truncate (!tempo *. speed_factor#value))
+           (if is_adjusted then (Printf.sprintf " (%d)" (truncate !tempo)) else ""))
     end
   end in
-
-  ignore (position#connect#value_changed ~callback:(fun () -> update_labels_for_position (truncate (position#value))));
+  let f () = (update_labels_for_position (truncate (position#value))) in
+  ignore (position#connect#value_changed ~callback:f);
 
   let load_file path = begin
     midi_state := Midi.read_midi_file path;
@@ -84,41 +125,42 @@ let main () =
     play_btn#misc#set_sensitive true;
   end in
 
-  let construct_measure num denom =
-    let dur = ((60. /. !tempo) /. ((float_of_int denom) /. 4.)) /. speed_factor#value in
-    let a = Array.make num (!root_pitch,dur) in
-    a.(0) <- (fifth !root_pitch,dur);
-    a
-    in
   let last_position = ref position#value in
-  ignore (play_btn#connect#clicked ~callback:(fun () ->
-    if play_btn#active then last_position := position#value else position#set_value !last_position;
+  let f () = begin
+    if play_btn#active then
+      last_position := position#value
+    else
+      position#set_value !last_position;
     let bar_count = ref 0 in
     let next_bar = ref (truncate position#value) in
-    let fresh_bar () = begin
+    let fresh_bar () =
+      let open Midi in
       incr bar_count;
-      if !bar_count <= truncate (preroll_bars#value) then begin
-	construct_measure (truncate (preroll_numerator#value)) (truncate (preroll_denominator#value))
-      end else begin
-	position#set_value (float_of_int !next_bar);
-	GtkThread.async (fun () -> update_labels_for_position (truncate position#value)) ();
-	if position#value >= position#upper then begin
-	  play_btn#set_active false;
-	  [| (0.,1.) |]
-	end else (incr next_bar; construct_measure (!meter).Midi.numerator (!meter).Midi.denominator)
-      end
-    end in
-    play_btn#set_label (GtkStock.convert_id (if play_btn#active then `MEDIA_STOP else `MEDIA_PLAY));
-    GtkThread.async (if play_btn#active then (fun () -> Metronome.start fresh_bar) else Metronome.stop) ()));
+      if !bar_count <= truncate (preroll.bars#value) then
+        construct_measure {numerator=(truncate (preroll.numerator#value));
+                           denominator=(truncate (preroll.denominator#value))}
+          speed_factor#value
+      else begin
+        position#set_value (float_of_int !next_bar);
+        GtkThread.async (fun () -> update_labels_for_position (truncate position#value)) ();
+        if position#value >= position#upper then begin
+          play_btn#set_active false;
+          [| (0.,1.) |]
+        end else begin
+          incr next_bar;
+          construct_measure !meter speed_factor#value
+        end
+      end in
+    (if play_btn#active then `MEDIA_STOP else `MEDIA_PLAY)
+    |> GtkStock.convert_id |> play_btn#set_label;
+    let f = if play_btn#active then
+        (fun () -> Metronome.start fresh_bar)
+      else Metronome.stop in
+    GtkThread.async f ()
+  end in
+  ignore (play_btn#connect#clicked ~callback:f);
 
-  List.iter (fun (l,f) -> let i = GMenu.menu_item ~label:l ~packing:file_menu#append () in
-                          ignore (i#connect#activate ~callback:f); ())
-    [("Open",(fun () ->
-      let chooser = GWindow.file_selection ~title:"Choose MIDI file" () in
-      ignore (chooser#ok_button#connect#clicked ~callback:(fun () -> load_file chooser#filename; chooser#destroy ()));
-      ignore (chooser#cancel_button#connect#clicked ~callback:chooser#destroy);
-      chooser#show ()));
-     ("Quit", GMain.Main.quit)];
+  construct_file_menu menu_bar load_file;
 
   Metronome.init ();
   window#show ();
